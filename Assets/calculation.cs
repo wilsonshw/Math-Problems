99ca386cb701cc477856faba5bc25971bfa0604a
Wilson <wilsonshw83@gmail.com>
2021-04-07 01:03:06 +0800

    Test1
    Test1 for github

---
+++ Assets/calculation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class calculation : MonoBehaviour
+{
+    float firstNo;
+    float secondNo;
+    float sumNow;
+    float evenSum;
+
+    private void Start()
+    {
+        firstNo = 1;
+        secondNo = 2;
+
+        do
+        {
+            if (secondNo % 2 == 0)
+            {
+                evenSum += secondNo;
+            }
+
+            sumNow = firstNo + secondNo;
+            firstNo = secondNo;
+            secondNo = sumNow;
+
+
+
+        } while (secondNo < 4000000);
+
+        Debug.Log(evenSum);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/clickevent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class clickevent : MonoBehaviour, IPointerDownHandler
+{
+    public Image selfImage;
+
+    public void OnPointerDown(PointerEventData ped)
+    {
+        Debug.Log("aaa");
+    }
+}
